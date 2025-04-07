e5cf9be2ad9b066f4c035a76566d372646494ba1
Gabriel Rial <grial@c3a9c1.42berlin.de>
2025-04-07 15:48:11 +0200

    render fixed

--- sources/ray_casting/rc_fov.c
+++ sources/ray_casting/rc_fov.c
@@ -6,7 +6,7 @@
 /*   By: grial <grial@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 18:55:33 by grial             #+#    #+#             */
-/*   Updated: 2025/04/07 15:33:31 by grial            ###   ########.fr       */
+/*   Updated: 2025/04/07 15:47:24 by grial            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -112,42 +112,46 @@ int get_texture_offset(float scale)
 void	draw_wall(t_game *game, int x_width, float x, float y, float ang)
 {
 	float	height;
-	int		tx_start;
-	int		tx_end;
-	int		steps;
-	float	scale;
+	int		draw_start;
+	int		draw_end;
 	t_img	*texture;
 	int		color;
-	
-	int		y_pos = 0;
-	int		tex_x;
-	int		tex_ys;
 
-	steps = (float)BLOCK / WIN_H;
+	int		screen_y;
+	int		tex_x;
+	int		tex_y;
+	float	step_tex_y;
+	float	tex_pos;
+
 	height = get_height(game, x, y, ang);
-	if (height > WIN_H)
-		scale = height / (float)WIN_H;
-	else 
-		scale = 1;
-	tx_start = get_texture_offset(scale); // when scale is 2, this is 16
-	tx_end = tx_start + (BLOCK / 2);
-	if (scale <= 1)
-	{
-		tx_start = 0;
-		tx_end = BLOCK;
-	}
-	tex_x = ((int)x % BLOCK); // o según la dirección
+	draw_start = (WIN_H / 2) - (height / 2);
+	if (draw_start < 0)
+		draw_start = 0;
+	draw_end = draw_start + height;
+	if (draw_end > WIN_H)
+		draw_end = WIN_H;
+
+	// Selección de textura (por ahora solo norte)
 	texture = game->engine->no_img;
-	while (tx_start < tx_end)
+	tex_x = (int)(x * 64) % 64;
+
+	// Relación entre altura de pared en pantalla y textura
+	step_tex_y = 64.0f / height;
+	tex_pos = (draw_start - WIN_H / 2 + height / 2) * step_tex_y;
+
+	screen_y = draw_start;
+	while (screen_y < draw_end)
 	{
-		tex_ys = ((steps * y_pos) / scale) + tx_start;
-		color = get_pixel_color(texture, tex_x, y);
-		// Dibujar el píxel en pantalla
-		my_mlx_pixel_put(game, x_width, y_pos, color);
-		y_pos++;
-		tx_start++;
-	} 
+		tex_y = (int)tex_pos & (64 - 1); // asumiendo textura 64x64
+		tex_pos += step_tex_y;
+
+		color = get_pixel_color(texture, tex_x, tex_y);
+		my_mlx_pixel_put(game, x_width, screen_y, color);
+		screen_y++;
+	}
 }
+
+
 
 
 int	draw_check_collision(t_game *game, int x_width, float x, float y, float ang)
