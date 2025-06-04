46bcbc41703c8c98aec59d9310c8fbbac000b0a0
Gabriel Rial <grial@c3a10c4.42berlin.de>
2025-06-04 16:17:08 +0200

    Working on leaks

--- sources/free/free_engine.c
+++ sources/free/free_engine.c
@@ -6,60 +6,50 @@
 /*   By: grial <grial@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 18:48:14 by tkasapog          #+#    #+#             */
-/*   Updated: 2025/06/04 15:07:40 by grial            ###   ########.fr       */
+/*   Updated: 2025/06/04 16:13:11 by grial            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/cub3d.h"
 
-void	free_engine(t_game *game)
+void	free_engine_resources(t_engine *engine, void *mlx_ptr)
 {
-	t_engine *ptr;
+	free_img(mlx_ptr, &engine->frame);
+	free_img(mlx_ptr, &engine->no_img);
+	free_img(mlx_ptr, &engine->so_img);
+	free_img(mlx_ptr, &engine->we_img);
+	free_img(mlx_ptr, &engine->ea_img);
+	free_img(mlx_ptr, &engine->dr_img);
 
-	if (!game->engine)
-		return ;
-	ptr = game->engine;
+	free_color(&engine->floor_color);
+	free_color(&engine->ceiling_color);
 
-	if (ptr->ray)
-		free(ptr->ray);
-	ptr->ray = NULL;
-	if (ptr->rcast)
-		free(ptr->rcast);
-	ptr->rcast = NULL;
-	free_engine_images(ptr);
-	
-	
-	
+	free(engine->no_path);
+	free(engine->so_path);
+	free(engine->we_path);
+	free(engine->ea_path);
+	free(engine->dr_path);
+
+	engine->no_path = NULL;
+	engine->so_path = NULL;
+	engine->we_path = NULL;
+	engine->ea_path = NULL;
+	engine->dr_path = NULL;
 }
 
-static void	free_engine_images1(t_engine *engine, void *mlx_ptr)
+static void	free_img(void *mlx_ptr, t_img **img)
 {
-
-}
-
-void	free_engine_images(t_engine *engine, void *mlx_ptr)
-{
-	free_engine_images1(engine, mlx_ptr);
-	if (engine->ea_img)
+	if (*img)
 	{
-		if (engine->ea_img->img)
-			mlx_destroy_image(mlx_ptr, engine->ea_img->img);
-		free(engine->ea_img);
-		engine->ea_img = NULL;
-	}
-	if (engine->frame)
-	{
-		if (engine->frame->img)
-			mlx_destroy_image(mlx_ptr, engine->frame->img);
-		free(engine->frame);
-		engine->frame = NULL;
+		if ((*img)->img)
+			mlx_destroy_image(mlx_ptr, (*img)->img);
+		free(*img);
+		*img = NULL;
 	}
 }
 
-void	free_engine_texture(t_engine *engine)
+static void	free_texture_path(t_engine *engine)
 {
-	if (!engine)
-		return ;
 	if (engine->no_path)
 		free(engine->no_path);
 	if (engine->so_path)
@@ -70,11 +60,12 @@ void	free_engine_texture(t_engine *engine)
 		free(engine->ea_path);
 }
 
-void	handle_error(t_game *game, const char *error_message)
+static void	free_color(t_color **color)
 {
-	printf("Error\n%s\n", error_message);
-	game->error = 1;
-	if (game)
-		free_game(game);
-	exit(1);
+	if (*color)
+	{
+		free(*color);
+		*color = NULL;
+	}
 }
+
